790aa794fdfff9bb8f10ef299cd91d01e0e266cb
Robin Farmer <robin@klippa.com>
2023-01-10 14:42:00 +0100

    update README.md & workflows.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,37 +1,16 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text, Button, Alert } from 'react-native';
-import { IdentityBuilder, startSession, KIVLanguage } from 'react-native-klippa-identity-verification-sdk';
-
-
-
+import { IdentityBuilder, startSession } from 'react-native-klippa-identity-verification-sdk';
 
 export default function App() {
   const [result, setResult] = React.useState<string | undefined>();
 
   function _startSession() {
 
-    const builder = new IdentityBuilder()
+    const identityBuilder = new IdentityBuilder()
 
-
-    builder.language = KIVLanguage.English
-    builder.colors.textColor = "#A020F0"
-    builder.colors.backgroundColor = "#262c96"
-    builder.colors.buttonErrorColor = "#967126"
-    builder.colors.buttonOtherColor = "#967126"
-    builder.colors.buttonSuccessColor = "#352696"
-    builder.colors.progressBarBackground = "#090521"
-    builder.colors.progressBarForeground = "#e6c5d3"
-
-
-    builder.hasIntroScreen = true
-    builder.hasSuccessScreen = true
-
-    builder.verifyIncludeList = ["Surname"]
-    builder.verifyExcludeList = ["Face"]
-
-
-    startSession(builder, "{your-session-token}")
+    startSession(identityBuilder, "{your-session-token}")
       .then(() => {
         setResult("Finished")
       })
