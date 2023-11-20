4102e9dad9289fbbadc96a095f44e5935afae99f
Robin Farmer <robin@klippa.com>
2023-11-20 09:52:36 +0100

    bump android to 0.7.0

--- android/src/main/java/com/klippaidentityverificationsdk/KlippaIdentityVerificationSdkModule.kt
+++ android/src/main/java/com/klippaidentityverificationsdk/KlippaIdentityVerificationSdkModule.kt
@@ -1,24 +1,65 @@
 package com.klippaidentityverificationsdk
 
+import android.app.Activity
+import android.content.Intent
+import com.facebook.react.bridge.BaseActivityEventListener
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
-import com.klippa.identity_verification.model.KlippaError
-import com.klippa.identity_verification.modules.base.IdentityBuilder
+import com.klippa.identity_verification.modules.base.IdentitySession
+import com.klippa.identity_verification.modules.base.IdentitySessionResultCode
 
 class KlippaIdentityVerificationSdkModule(private val reactContext: ReactApplicationContext) :
-    ReactContextBaseJavaModule(reactContext), IdentityBuilder.IdentityBuilderListener {
+    ReactContextBaseJavaModule(reactContext) {
 
-    private val E_UNKNOWN = "E_UNKNOWN_ERROR"
-    private val E_CANCELED = "E_CANCELED"
-    private val E_SUPPORT_PRESSED = "E_SUPPORT_PRESSED"
+    companion object {
+        private const val NAME = "KlippaIdentityVerificationSdk"
+        private const val REQUEST_CODE = 99991801
+
+        private const val E_CANCELED = "E_CANCELED"
+        private const val E_SUPPORT_PRESSED = "E_SUPPORT_PRESSED"
+    }
 
     private var _promise: Promise? = null
 
     override fun getName(): String {
         return NAME
+    }
+
+    private val activityEventListener = object : BaseActivityEventListener() {
+        override fun onActivityResult(
+            activity: Activity?,
+            requestCode: Int,
+            resultCode: Int,
+            data: Intent?
+        ) {
+            super.onActivityResult(activity, requestCode, resultCode, data)
+
+            val mappedResultCode = IdentitySessionResultCode.mapResultCode(resultCode)
+            when (mappedResultCode) {
+                IdentitySessionResultCode.FINISHED -> identityVerificationFinished()
+                IdentitySessionResultCode.CONTACT_SUPPORT_PRESSED -> identityVerificationContactSupportPressed(
+                    mappedResultCode.message()
+                )
+
+                IdentitySessionResultCode.INSUFFICIENT_PERMISSIONS,
+                IdentitySessionResultCode.INVALID_SESSION_TOKEN,
+                IdentitySessionResultCode.USER_CANCELED,
+                IdentitySessionResultCode.NO_INTERNET_CONNECTION,
+                IdentitySessionResultCode.DEVICE_DOES_NOT_SUPPORT_NFC,
+                IdentitySessionResultCode.DEVICE_NFC_DISABLED,
+                IdentitySessionResultCode.UNKNOWN_ERROR,
+                IdentitySessionResultCode.TAKING_PHOTO_FAILED -> identityVerificationCanceled(
+                    mappedResultCode.message()
+                )
+            }
+        }
+    }
+
+    init {
+        reactContext.addActivityEventListener(activityEventListener)
     }
 
     @ReactMethod
@@ -32,107 +73,89 @@ class KlippaIdentityVerificationSdkModule(private val reactContext: ReactApplica
         val builder = setupIdentityBuilder(sessionToken, config.toHashMap())
 
         val intent = builder.getIntent(reactContext)
-
-        currentActivity?.startActivity(intent)
-
+        currentActivity?.startActivityForResult(intent, REQUEST_CODE)
     }
 
-    private fun setupIdentityBuilder(sessionToken: String, config: Map<String, Any>) : IdentityBuilder {
-        val builder = IdentityBuilder(this, sessionToken)
 
-        setBuilderLanguage(config, builder)
+    private fun setupIdentityBuilder(
+        sessionToken: String,
+        config: Map<String, Any>
+    ): IdentitySession {
+        val identitySession = IdentitySession(sessionToken)
 
-        setBuilderOptionalScreens(config, builder)
+        setBuilderLanguage(config, identitySession)
+
+        setBuilderOptionalScreens(config, identitySession)
 
         (config["isDebug"] as? Boolean)?.also { isDebug ->
-            builder.isDebug = isDebug
+            identitySession.isDebug = isDebug
         }
 
-        setVerificationLists(config, builder)
+        (config["retryThreshold"] as? Double)?.let { retryThreshold ->
+            identitySession.retryThreshold = retryThreshold.toInt()
+        }
 
-        return builder
+        setVerificationLists(config, identitySession)
+
+
+        return identitySession
     }
 
     private fun setVerificationLists(
         config: Map<String, Any>,
-        builder: IdentityBuilder
+        identitySession: IdentitySession
     ) {
         @Suppress("UNCHECKED_CAST")
         (config["verifyIncludeList"] as? List<String>)?.also { verifyIncludeList ->
-            builder.kivIncludeList = verifyIncludeList
+            identitySession.kivIncludeList = verifyIncludeList
         }
 
         @Suppress("UNCHECKED_CAST")
         (config["verifyExcludeList"] as? List<String>)?.also { verifyExcludeList ->
-            builder.kivExcludeList = verifyExcludeList
+            identitySession.kivExcludeList = verifyExcludeList
         }
     }
 
     private fun setBuilderOptionalScreens(
         config: Map<String, Any>,
-        builder: IdentityBuilder
+        identitySession: IdentitySession
     ) {
         (config["hasIntroScreen"] as? Boolean)?.also { hasIntroScreen ->
-            builder.hasIntroScreen = hasIntroScreen
+            identitySession.hasIntroScreen = hasIntroScreen
         }
 
         (config["hasSuccessScreen"] as? Boolean)?.also { hasSuccessScreen ->
-            builder.hasSuccessScreen = hasSuccessScreen
+            identitySession.hasSuccessScreen = hasSuccessScreen
         }
     }
 
     private fun setBuilderLanguage(
         config: Map<String, Any>,
-        builder: IdentityBuilder
+        identitySession: IdentitySession
     ) {
         (config["language"] as? String)?.also { language ->
             when (language) {
-                "English" -> builder.language = IdentityBuilder.KIVLanguage.English
-                "Dutch" -> builder.language = IdentityBuilder.KIVLanguage.Dutch
-                "Spanish" -> builder.language = IdentityBuilder.KIVLanguage.Spanish
+                "English" -> identitySession.language = IdentitySession.KIVLanguage.English
+                "Dutch" -> identitySession.language = IdentitySession.KIVLanguage.Dutch
+                "Spanish" -> identitySession.language = IdentitySession.KIVLanguage.Spanish
             }
         }
     }
 
-    companion object {
-        const val NAME = "KlippaIdentityVerificationSdk"
-    }
-
-    override fun identityVerificationFinished() {
+    private fun identityVerificationFinished() {
         _promise?.resolve(null)
         _promise = null
     }
 
-    override fun identityVerificationCanceled(error: KlippaError) {
-        when (error) {
-            KlippaError.InsufficientPermissions -> {
-                _promise?.reject(E_CANCELED, "Insufficient permissions")
-            }
-            KlippaError.InvalidSessionToken -> {
-                _promise?.reject(E_CANCELED, "Invalid session token")
-            }
-            KlippaError.UserCanceled -> {
-                _promise?.reject(E_CANCELED, "User canceled session")
-            }
-            KlippaError.NoInternetConnection -> {
-                _promise?.reject(E_CANCELED, "No active internet connection")
-            }
-            KlippaError.DeviceDoesNotSupportNFC -> {
-                _promise?.reject(E_CANCELED, "Device does not support NFC")
-            }
-            KlippaError.DeviceNFCDisabled -> {
-                _promise?.reject(E_CANCELED, "NFC is disabled on device")
-            }
-            else -> {
-                _promise?.reject(E_UNKNOWN, "Failed with unknown error")
-            }
-        }
+
+    private fun identityVerificationCanceled(message: String) {
+        _promise?.reject(E_CANCELED, message)
         _promise = null
     }
 
-    override fun identityVerificationContactSupportPressed() {
-        _promise?.reject(E_SUPPORT_PRESSED, "Contact support pressed")
-        _promise =  null
+    private fun identityVerificationContactSupportPressed(message: String) {
+        _promise?.reject(E_SUPPORT_PRESSED, message, null)
+        _promise = null
     }
 
 }
